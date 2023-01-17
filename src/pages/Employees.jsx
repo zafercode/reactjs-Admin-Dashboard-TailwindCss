276420c79cb331648adc1a59e6b4fd1478170328
Zafer Kalik <zaferkalik@gmail.com>
2023-01-17 16:47:17 +0300

    first commit

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Search, Page, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Employees = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
+};
+export default Employees;
